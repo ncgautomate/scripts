3edd91d0890138a9f9ca8b8bdf42988f12d94403
ifazalam1 <ifaz.alam1@gmail.com>
2023-12-07 01:20:22 +0600

    v5 changes ifaz for title

--- wordpress-md-post-convert/md-csv-conversion-script-v5.py
+++ wordpress-md-post-convert/md-csv-conversion-script-v5.py
@@ -8,7 +8,6 @@ from collections import Counter
 import string
 
 
-
 # Define your CSS content for styling the HTML
 css_content = """
 /* Table styles */
@@ -57,14 +56,15 @@ html_template = """<!DOCTYPE html>
 
 # Markdown conversion function with necessary extensions
 # Markdown conversion function with necessary extensions
+
+
 def convert_md_to_html(md_content, title):
     extensions = ['extra', 'abbr', 'tables', 'sane_lists', 'codehilite']
     md = markdown.Markdown(extensions=extensions)
     md_html_content = md.convert(md_content)
-    final_html_content = html_template.format(title=title, md_html_content=md_html_content, css_content=css_content)
+    final_html_content = html_template.format(
+        title=title, md_html_content=md_html_content, css_content=css_content)
     return final_html_content
-
-
 
 
 # Function to create a URL-friendly slug
@@ -73,39 +73,79 @@ def create_slug(title):
     return re.sub(r'[\s-]+', '-', title).strip('-')
 
 # Function to extract keyphrases based on the most-used non-stop word in the content
+
+
 def extract_keyphrases(content):
-    words = content.translate(str.maketrans('', '', string.punctuation)).split()
-    stop_words = set(['and', 'the', 'of', 'to', 'a', 'in', 'for', 'is', 'on', 'that', 'by', 'this', 'with', 'i', 'you', 'it', 'not', 'or', 'be', 'are', 'from', 'at', 'as', 'your', 'have', 'more', 'can', 'an', 'was', 'we'])
+    words = content.translate(str.maketrans(
+        '', '', string.punctuation)).split()
+    stop_words = set(['and', 'the', 'of', 'to', 'a', 'in', 'for', 'is', 'on', 'that', 'by', 'this', 'with', 'i',
+                     'you', 'it', 'not', 'or', 'be', 'are', 'from', 'at', 'as', 'your', 'have', 'more', 'can', 'an', 'was', 'we'])
     words = [word for word in words if word not in stop_words]
     most_common_word = Counter(words).most_common(1)[0][0]
     return most_common_word
 
 # Function to process each markdown file
+
+
 def process_markdown_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         md_content = file.read()  # Read the entire Markdown file content
 
-    # Extract the title from the Markdown content (assuming it's the first line)
+    # Extract the title from the Markdown content
     md_lines = md_content.split('\n')
-    title_match = re.match(r'^# (.+)', md_lines[0])
-    title = title_match.group(1).strip() if title_match else "Default Title"
-    title = re.sub(r'[^\w\s-]', '', title)  # Remove special characters from title
+
+    # Search for a line with # or ## at the beginning, consider the first 3 lines
+    title_match = None
+    for i in range(min(3, len(md_lines))):
+        match = re.match(r'^(#+) (.+)', md_lines[i])
+        if match:
+            title_match = match
+            break
+
+    # If no match found, use a default title
+    if not title_match:
+        title = "Default Title"
+    else:
+        title_level, title = title_match.groups()
+
+        # Remove the # or ## prefix from the title
+        title = title.strip()
+        title = re.sub(r'^#+\s*', '', title)  # Remove # or ## at the beginning
+
+        # Adjust title level for SEO tags removal
+        title_level = min(3, len(md_lines))  # Maximum title level is 3
+        md_lines = md_lines[title_level:]
+
+    # Find SEO tags using the keyword "SEO" in a case-insensitive manner
+    seo_tags_match = re.search(r'\bSEO\b', md_content, re.IGNORECASE)
+
+    if seo_tags_match:
+        seo_tags_start = seo_tags_match.start()
+        # Assume tags end with a new line
+        seo_tags_end = md_content.find('\n', seo_tags_start)
+        seo_tags = md_content[seo_tags_start:seo_tags_end].strip()
+    else:
+        seo_tags = ""
 
     # Prepare the Markdown content excluding the title line for HTML conversion
-    md_content_without_title_and_seo = '\n'.join(md_lines[1:])  # Exclude the first line which is the title
-    md_content_without_title_and_seo = re.sub(r'#### SEO High-Ranking Page Tags\n.*', '', md_content_without_title_and_seo, flags=re.DOTALL)
-
+    md_content_without_title_and_seo = '\n'.join(md_lines)
+    md_content_without_title_and_seo = re.sub(
+        r'\bSEO\b.*', '', md_content_without_title_and_seo, flags=re.IGNORECASE)
 
     # Convert the Markdown content to HTML
     html_content = convert_md_to_html(md_content_without_title_and_seo, title)
-    
+
     # Remove the <title> tag content and anything after "SEO High-Ranking Page Tags"
-    html_content = re.sub(r'<title>.*</title>', '', html_content, flags=re.DOTALL)
-    html_content = re.sub(r'<h4>SEO High-Ranking Page Tags</h4>.*', '', html_content, flags=re.DOTALL)
+    html_content = re.sub(r'<title>.*</title>', '',
+                          html_content, flags=re.DOTALL)
+    html_content = re.sub(
+        r'<h4>SEO High-Ranking Page Tags</h4>.*', '', html_content, flags=re.DOTALL)
+
+    # ... (rest of the function remains unchanged)
 
     # Initialize default values
     post_author_default = 'trioadmin'
-    #post_date_default = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    # post_date_default = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     post_status_default = 'publish'
     featured_image_default = 'default_image_url'
     editors_choice_default = '1'
@@ -113,7 +153,8 @@ def process_markdown_file(file_path):
     keep_trending_default = '1'
 
     # Extract SEO tags from the original Markdown content
-    seo_tags_match = re.search(r'#### SEO High-Ranking Page Tags\n(.+)', md_content, re.DOTALL)
+    seo_tags_match = re.search(
+        r'#### SEO High-Ranking Page Tags\n(.+)', md_content, re.DOTALL)
     seo_tags = seo_tags_match.group(1).strip() if seo_tags_match else ""
 
     # Generate post_name from the title
@@ -124,7 +165,7 @@ def process_markdown_file(file_path):
         'post_title': title,
         'post_content': html_content,  # Using converted HTML content
         'post_excerpt': html_content[:100] + '...' if len(html_content) > 100 else html_content,
-        'post_date': '', #post_date_default,
+        'post_date': '',  # post_date_default,
         'post_name': post_name,
         'post_author': post_author_default,
         'post_status': post_status_default,
@@ -142,7 +183,7 @@ def process_markdown_file(file_path):
         'robots_max_imagepreview': 'large',
         'keyphrases': extract_keyphrases(html_content),
         'post_category': 'AI',
-        'post_tags': seo_tags,
+        'post_tag': seo_tags,
         'editors_choice': editors_choice_default,
         'editors_note': '',
         'featured-post': featured_post_default,
@@ -155,7 +196,7 @@ def process_markdown_file(file_path):
 
 
 def main():
-    directory = 'E:/Github/orgs/ncgcloudhub/scripts/wordpress-md-post-convert/'
+    directory = 'D:/TrionxAI/scripts/wordpress-md-post-convert/'
     headers_csv_path = os.path.join(directory, 'headers-only.csv')
     markdown_files = glob.glob(os.path.join(directory, '*.md'))
     output_csv_file_name = 'output.csv'
@@ -171,21 +212,21 @@ def main():
     # Write headers if needed and data to the CSV file
     with open(csv_file_path, 'a', newline='', encoding='utf-8') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=headers)
-        
+
         if needs_header:
             writer.writeheader()
-        
+
         for md_file_path in markdown_files:
             csv_row = process_markdown_file(md_file_path)
 
             # Ensure csv_row has keys exactly matching the headers
-            row_to_write = {header: csv_row.get(header, '') for header in headers}
-            
+            row_to_write = {header: csv_row.get(
+                header, '') for header in headers}
+
             writer.writerow(row_to_write)
 
     print(f"CSV file saved as: {csv_file_path}")
 
+
 if __name__ == '__main__':
     main()
-
-
--- wordpress-md-post-convert/md-csv-conversion-script.py
+++ wordpress-md-post-convert/md-csv-conversion-script.py
@@ -18,6 +18,8 @@ stop_words = set("""a about above after again against all am an and any are aren
                     yourself yourselves""".split())
 
 # Function to calculate most used word for keyphrases
+
+
 def calculate_keyphrases(content):
     words = [word.lower() for word in re.findall(r'\w+', content)]
     words = [word for word in words if word not in stop_words]
@@ -26,36 +28,46 @@ def calculate_keyphrases(content):
     return most_common_word[0][0] if most_common_word else ''
 
 # Function to generate post_name from post_title
+
+
 def generate_post_name(title):
     return re.sub(r'\s+', '-', title).lower()
 
 # Function to extract tags from markdown
+
+
 def extract_tags(content):
     # Assuming tags are at the end of the content in the format "SEO High-Ranking Page Tags: tag1, tag2, ..."
     match = re.search(r"SEO High-Ranking Page Tags:(.*)", content)
     return match.group(1).strip() if match else ''
 
 # Function to parse markdown content
+
+
 def parse_markdown(md_content):
     lines = md_content.splitlines()
-    
+
     # Assuming the first line is the title in format "Title: Some Title"
     title = lines[0].replace("Title:", "").strip()
 
     # The rest is the content, which ends with the Tags designated by a specific line
-    end_of_content = next((i for i, line in enumerate(lines) if line.startswith("Follow our Social Media")), len(lines))
+    end_of_content = next((i for i, line in enumerate(
+        lines) if line.startswith("Follow our Social Media")), len(lines))
     content = "".join(lines[1:end_of_content]).strip()
 
     # Extract tags
     tags = extract_tags(content)
 
     # Clear tags from content
-    content = content.replace(f"SEO High-Ranking Page Tags: {tags}", "").strip()
+    content = content.replace(
+        f"SEO High-Ranking Page Tags: {tags}", "").strip()
 
     return title, content, tags
 
+
 # Read the Markdown file content
-md_content_path = 'E:/Github/orgs/ncgcloudhub/scripts/wordpress-md-post-convertadobefirefly.md'#'/mnt/data/file-UuGgKDfbcg3YQGVhC4CzNHhq'
+# '/mnt/data/file-UuGgKDfbcg3YQGVhC4CzNHhq'
+md_content_path = 'E:/Github/orgs/ncgcloudhub/scripts/wordpress-md-post-convertadobefirefly.md'
 with open(md_content_path, 'r', encoding='utf-8') as md_file:
     md_content = md_file.read()
 
@@ -77,7 +89,8 @@ csv_row = {
     "post_status": "",  # Will be determined once all mandatory fields are checked
     "featured_image": "",  # To be left blank
     "keyphrases": keyphrases,
-    "post_category": "Default Category",  # Replace with actual category provided by the user
+    # Replace with actual category provided by the user
+    "post_category": "Default Category",
     "post_tag": tags
 }
 
@@ -85,7 +98,8 @@ csv_row = {
 output_csv_path = 'output.csv'
 
 # Define the list of mandatory fields
-mandatory_fields = ["post_title", "post_content", "post_name", "post_author", "post_category", "post_tag"]
+mandatory_fields = ["post_title", "post_content",
+                    "post_name", "post_author", "post_category", "post_tag"]
 
 # Set post_status to 'publish' if all mandatory fields are present and filled
 if all(csv_row[field] for field in mandatory_fields):
@@ -101,7 +115,8 @@ csv_output.append([csv_row[header] for header in csv_headers])
 
 # For demonstration, let's print out what would be written to the CSV
 csv_output
-[['post_title', 'post_content', 'post_name', 'post_author', 'post_status', 'featured_image', 'keyphrases', 'post_category', 'post_tag'], ["### Igniting Creativity: Exploring Adobe Firefly's Creative Revolution", "#### SummaryAdobe Firefly emerges as a powerful creative tool, redefining digital design and collaboration. This article delves into its key features, highlights pros and cons, offers valuable tips, and presents real-world examples showcasing Adobe Firefly's transformative capabilities.#### Key Points- Seamless Collaboration- Intuitive Design Interface- Cloud-Based Workflow- Extensive Asset Library#### Pros and Cons| Pros | Cons ||--------------------------|-------------------------------|| Robust Collaboration | Learning Curve for Beginners || Intuitive User Interface | Subscription-based Model || Accessible Anywhere | Advanced Features Require Pro Plan || Comprehensive Asset Library | Internet Dependency |#### Tips for the Reader 💡Enhance your Adobe Firefly experience with these tips:- Explore the in-app tutorials for a smoother onboarding process.- Leverage collaboration features to streamline team projects.- Save designs regularly to the cloud for easy access across devices.#### Examples##### Example 1: Collaborative Design Session**Prompt:** Creative Collaboration with Adobe Firefly**Input:**```dartimport firefly_adobeproject = firefly_adobe.create_project('TeamDesign')firefly_adobe.invite_collaborators(project, ['designer1', 'designer2'])```**Output:**```dartReal-time collaboration initiated: Designers working seamlessly on the same project.```##### Example 2: Designing on the Go**Prompt:** Mobile Design with Adobe Firefly**Input:**```dartimport firefly_adobedesign = firefly_adobe.create_design('OnTheGo')firefly_adobe.export_to_cloud(design)```**Output:**```dartDesign exported to the cloud: Accessible from any device, anywhere.```👉 [Try for yourself](https://firefly.adobe.com/)#### URL Address of the AI Topic / Vendor[Adobe Firefly](https://firefly.adobe.com/)---", "###-igniting-creativity:-exploring-adobe-firefly's-creative-revolution", 'trioadmin', '', '', 'firefly', 'Default Category', '']]
+[['post_title', 'post_content', 'post_name', 'post_author', 'post_status', 'featured_image', 'keyphrases', 'post_category', 'post_tag'], ["### Igniting Creativity: Exploring Adobe Firefly's Creative Revolution",
+                                                                                                                                          "#### SummaryAdobe Firefly emerges as a powerful creative tool, redefining digital design and collaboration. This article delves into its key features, highlights pros and cons, offers valuable tips, and presents real-world examples showcasing Adobe Firefly's transformative capabilities.#### Key Points- Seamless Collaboration- Intuitive Design Interface- Cloud-Based Workflow- Extensive Asset Library#### Pros and Cons| Pros | Cons ||--------------------------|-------------------------------|| Robust Collaboration | Learning Curve for Beginners || Intuitive User Interface | Subscription-based Model || Accessible Anywhere | Advanced Features Require Pro Plan || Comprehensive Asset Library | Internet Dependency |#### Tips for the Reader 💡Enhance your Adobe Firefly experience with these tips:- Explore the in-app tutorials for a smoother onboarding process.- Leverage collaboration features to streamline team projects.- Save designs regularly to the cloud for easy access across devices.#### Examples##### Example 1: Collaborative Design Session**Prompt:** Creative Collaboration with Adobe Firefly**Input:**```dartimport firefly_adobeproject = firefly_adobe.create_project('TeamDesign')firefly_adobe.invite_collaborators(project, ['designer1', 'designer2'])```**Output:**```dartReal-time collaboration initiated: Designers working seamlessly on the same project.```##### Example 2: Designing on the Go**Prompt:** Mobile Design with Adobe Firefly**Input:**```dartimport firefly_adobedesign = firefly_adobe.create_design('OnTheGo')firefly_adobe.export_to_cloud(design)```**Output:**```dartDesign exported to the cloud: Accessible from any device, anywhere.```👉 [Try for yourself](https://firefly.adobe.com/)#### URL Address of the AI Topic / Vendor[Adobe Firefly](https://firefly.adobe.com/)---", "###-igniting-creativity:-exploring-adobe-firefly's-creative-revolution", 'trioadmin', '', '', 'firefly', 'Default Category', '']]
 
 # Write the CSV data to the file
 with open(output_csv_path, 'w', newline='', encoding='utf-8') as csvfile:
@@ -111,19 +126,19 @@ with open(output_csv_path, 'w', newline='', encoding='utf-8') as csvfile:
 print(f"CSV file has been written to {output_csv_path}")
 
 
-#Team-TrionxAI-Bot
-#The script has successfully generated the CSV content based on the markdown file you provided. The CSV content structure is represented as a list of lists, where the first sublist is the header row, and the following sublists contain the data extracted from the markdown content.
+# Team-TrionxAI-Bot
+# The script has successfully generated the CSV content based on the markdown file you provided. The CSV content structure is represented as a list of lists, where the first sublist is the header row, and the following sublists contain the data extracted from the markdown content.
 
-#However, since the post_status field relies on the input of some user-provided mandatory fields that cannot be collected through this script running in the current environment (like post_category), the post_status has been left empty. In a full Python environment, the script would prompt the user to provide these values, and upon receiving all necessary data, it would set post_status to "publish".
+# However, since the post_status field relies on the input of some user-provided mandatory fields that cannot be collected through this script running in the current environment (like post_category), the post_status has been left empty. In a full Python environment, the script would prompt the user to provide these values, and upon receiving all necessary data, it would set post_status to "publish".
 
-#Here's the summary of the CSV output content based on the provided markdown:
+# Here's the summary of the CSV output content based on the provided markdown:
 
-#The title has been extracted and formatted as specified in the markdown file.
-#The content has been extracted, excluding the "SEO High-Ranking Page Tags" at the end as per your instructions.
-#post_name has been generated from the post_title.
-#post_author is set to "trioadmin".
-#featured_image is left blank.
-#keyphrases have been calculated from the most frequently used non-stop word in the content, which is "firefly" in this case.
-#post_category is set to a placeholder value "Default Category" and should be replaced with actual input from the user.
-#post_tag is left blank as tags were not found in the provided example markdown content.
-#The final CSV content would be written to a file if executed in a full Python environment. You can take this output and adapt it to a local script that would prompt for missing mandatory fields and write to a CSV file accordingly.
+# The title has been extracted and formatted as specified in the markdown file.
+# The content has been extracted, excluding the "SEO High-Ranking Page Tags" at the end as per your instructions.
+# post_name has been generated from the post_title.
+# post_author is set to "trioadmin".
+# featured_image is left blank.
+# keyphrases have been calculated from the most frequently used non-stop word in the content, which is "firefly" in this case.
+# post_category is set to a placeholder value "Default Category" and should be replaced with actual input from the user.
+# post_tag is left blank as tags were not found in the provided example markdown content.
+# The final CSV content would be written to a file if executed in a full Python environment. You can take this output and adapt it to a local script that would prompt for missing mandatory fields and write to a CSV file accordingly.
